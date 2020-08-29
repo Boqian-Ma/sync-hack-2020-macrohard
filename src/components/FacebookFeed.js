87f0f94c630b28a6858dd7d181717cf3918ef19a
Jeremy Lim <jeremy.lim1636@gmail.com>
2020-08-30 05:43:41 +1000

    Testing live feed

--- src/components/FacebookFeed.js
+++ src/components/FacebookFeed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,6 +12,8 @@ const action = (
   </Button>
 );
 
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: '100%',
@@ -23,14 +25,25 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function LongTextSnackbar() {
-  const classes = useStyles();
-
-  const data = ["28/7 | Facebook: Your profile has been used for market sizing.",
+  let datea = ["28/7 | Facebook: Your profile has been used for market sizing.",
   "28/7 | Facebook: Your location has been used for geographic advertisements.",
   "27/7 | Facebook: Your age has been used for product advertisements.",
   "28/7 | Facebook: Your profile has been used for market sizing.",
   "28/7 | Facebook: Your location has been used for geographic advertisements.",
-  "27/7 | Facebook: Your age has been used for product planning."]
+  ]
+  const classes = useStyles();
+  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(datea);
+
+  window.setInterval(() => {
+      if (counter < 10) {
+        setData(data.splice(0, 0, backUp[counter]));
+        setCounter(counter+1);
+        console.log(data);
+      }
+    }, 4000);
+
+  const backUp = ["1","2","3","4","5","6","7","8","9","10"]
 
   return (
     <div className={classes.root}>
