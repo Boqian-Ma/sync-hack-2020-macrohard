595cc1c189adc6af0653d4aca43e517bf1b5bd64
adammbq <adammbq@gmail.com>
2020-08-30 01:15:16 +1000

    yeet

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState }, { useEffect } from 'react'
 import { AccountCard, FacebookFeed, ManageAccount, PieGraph, SearchCompanies, Sidebar } from '../components';
 import { Link } from '@material-ui/core';
 import PerfectScrollbar from 'react-perfect-scrollbar'
