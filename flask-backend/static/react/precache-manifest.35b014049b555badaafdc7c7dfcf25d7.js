419201edf069d20e680e15f9dcf9c830467e741b
adammbq <adammbq@gmail.com>
2020-08-29 13:37:37 +1000

    init

---
+++ flask-backend/static/react/precache-manifest.35b014049b555badaafdc7c7dfcf25d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3062cd7bb2e11a501c538bb12831449",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "cebea04a7a504a75a33c",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "336b1d4b5bca64a64fdf",
+    "url": "/static/react/js/2.cf1e2948.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.cf1e2948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cebea04a7a504a75a33c",
+    "url": "/static/react/js/main.92410701.chunk.js"
+  },
+  {
+    "revision": "642bffd405cc52de04d1",
+    "url": "/static/react/js/runtime-main.8fa3ef7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
