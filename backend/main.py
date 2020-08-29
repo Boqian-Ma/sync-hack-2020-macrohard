689d59b5cb41a0825a016508804119dd97a23b45
adammbq <adammbq@gmail.com>
2020-08-29 23:17:44 +1000

    oppsi

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-import flask 
-import json
+import flask
+from json import dumps
 
 from data import *
 
@@ -9,33 +9,43 @@ def index():
     pass
     # return flask.render_template("index.html", token="Hello flask+react")
 
-@app.route("/login", methods=[POST, GET])
+@app.route("/login", methods=["POST", "GET"])
 def login():
     
     pass
 
-@app.route("/home", methods=[GET])
+@app.route("/home", methods=["GET"])
 def home():
     # dump user company list 
-    companies = DATA['users'] 
+    companies_list = []
+    for user in DATA['users']:
+        companies_list = user["accounts"]
+    print(companies_list)
     # dump past 7 day data for piechart
-
+    seven_data = get_frequency_week('adamma')
+    print(seven_data)
+    package = {'list' : companies_list, 'piechart': seven_data}
     # (optional) live update of 
-    pass
+    return dumps(package)
 
 
-@app.route("/accounts", methods=[GET])
+@app.route("/accounts", methods=["GET"])
 def accounts():
     # dump whole list of companies 
-    pass
+    my_companies = []
+    for user in DATA['users']:
+        my_companies = user["accounts"]
+    whole_list = companies
+    package = {'list' : whole_list, 'my_companies': my_companies}
+    print(package)
+    return dumps(package)
 
-@app.route("/accounts/<company>", methods=[GET])
+@app.route("/accounts/<company>", methods=["GET"])
 def company(company):
     # dump company data usage
 
     # dump indivisual comoany past 7 days data usage
     pass
-
 
 
 if __name__ == "__main__":
--- backend/test.py
+++ backend/test.py
@@ -1,13 +1,19 @@
 from data import *
-
+from main import *
 def test_adduser():
     print(new_user('lyric', '123456', 'lyric@gmail.com'))
 
 def test_addcompany():
     print(new_account('adamma', 'tiktok'))
-    
+
+def test_main():
+    home()
+
+def test_accounts():
+    accounts()
 
 
 if __name__ == "__main__":
     #test_adduser()
-    test_addcompany()
+    #test_addcompany()
+    test_accounts()
