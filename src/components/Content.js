9bb40f648dec913067356532550a07ced54b8aaa
matt-juan <matthew.juan23@gmail.com>
2020-08-30 01:19:00 +1000

    fixed import bug

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,4 @@
-import React, { useState }, { useEffect } from 'react'
+import React, { useState } from 'react'
 import { AccountCard, FacebookFeed, ManageAccount, PieGraph, SearchCompanies, Sidebar } from '../components';
 import { Link } from '@material-ui/core';
 
