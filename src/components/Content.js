853a6bf8567358e21db6f03bb35f502e7296dd4d
Jeremy Lim <jeremy.lim1636@gmail.com>
2020-08-29 18:55:30 +1000

    Added container styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react'
 import { Grid, Card, Button, Typography } from '@material-ui/core';
-import { Sidebar } from '../components';
-// import './Component.css'
+import { AccountCard, FacebookFeed, Sidebar } from '../components';
+import './Content.css'
 
-export default function Component(props) {
+export default function Content(props) {
     var accounts = ["Facebook", "Instagram", "Reddit", "Twitter"]
     return (
-        <Sidebar width={300} height={"100vh"}>
-            <h2>Dashboard</h2>
-            <h2>Accounts</h2>
-            {accounts.map((web, idx) => {
-                return(
-                    <div>
-                        <a>{web}</a>
-                    </div>
-                )
-            })}
-            
-        </Sidebar>
+        <div className="container">
+            <div className="side-container">
+                <Sidebar width={300} height={"100%"}>
+                    <h2>Dashboard</h2>
+                    <h2>Accounts</h2>
+                    {accounts.map((web, idx) => {
+                        return(
+                            <div>
+                                <a>{web}</a>
+                            </div>
+                        )
+                    })}
+                </Sidebar>
+            </div>
+            <div className="body-container">
+                <div className="top-container">
+                    <AccountCard className="item"></AccountCard>
+                    <AccountCard className="item"></AccountCard>
+                    <AccountCard className="item"></AccountCard>
+                    <AccountCard className="item"></AccountCard>
+                </div>
+                <div className="bottom-container">
+                    <FacebookFeed></FacebookFeed>
+                </div>
+            </div>
+        </div>
     )
 }
 
