6bf0f7c91d49a6868b1df70686c7b48ae736ab35
Jeremy Lim <jeremy.lim1636@gmail.com>
2020-08-29 14:49:11 +1000

    Added component structure

---
+++ react-frontend/src/components/Component.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { Grid, Card, Button, Typography } from '@material-ui/core';
+// import './Component.css'
+
+export default function Component(props) {
+    return(
+        <div></div>
+    )
+}
